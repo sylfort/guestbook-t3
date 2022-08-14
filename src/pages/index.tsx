8d9ac431ea51528dfb1324bed1452d1e91d013ba
Sylvio <sylviomafort@gmail.com>
2022-08-14 21:26:00 +0900

    added optimistic updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
+import { useState } from "react";
 
 import { signIn, signOut, useSession } from "next-auth/react";
 
@@ -23,7 +24,21 @@ const Messages = () => {
 
 const Home = () => {
   const { data: session, status } = useSession();
-  const postMessage = trpc.useMutation("guestbookpostMessage");
+  const [message, setMessage] = useState("");
+  const ctx = trpc.useContext();
+  const postMessage = trpc.useMutation("guestbookpostMessage", {
+    onMutate: () => {
+      ctx.cancelQuery(["guestbookgetAll"]);
+
+      let optimisticUpdate = ctx.getQueryData(["guestbookgetAll"]);
+      if (optimisticUpdate) {
+        ctx.setQueryData(["guestbookgetAll"], optimisticUpdate);
+      }
+    },
+    onSettled: () => {
+      ctx.invalidateQueries(["guestbookgetAll"]);
+    }
+  });
 
   if (status === "loading") {
     return <main className="flex flex-col items-center pt-4">Loading...</main>;
@@ -32,6 +47,9 @@ const Home = () => {
   return (
     <main className="flex flex-col items-center">
       <h1 className="text-3xl pt-4">Guestbook</h1>
+      <p>
+        Tutorial for <code>create-t3-app</code>
+      </p>
 
       <div className="pt-10">
         {session ? (
@@ -39,20 +57,56 @@ const Home = () => {
             <p>hi {session.user?.name}</p>
 
             <button onClick={() => signOut()}>Logout</button>
+
+            <div className="pt-6">
+              <form
+                className="flex gap-2"
+                onSubmit={(event) => {
+                  event.preventDefault();
+
+                  postMessage.mutate({
+                    name: session.user?.name as string,
+                    message,
+                  });
+
+                  setMessage("");
+                }}
+              >
+                <input
+                  type="text"
+                  value={message}
+                  placeholder="Your message..."
+                  maxLength={100}
+                  onChange={(event) => setMessage(event.target.value)}
+                  className="px-4 py-2 rounded-md border-2 border-zinc-800 bg-neutral-900 focus:outline-none"
+                />
+                <button
+                  type="submit"
+                  className="p-2 rounded-md border-2 border-zinc-800 focus:outline-none"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+
+            <div className="pt-10">
+              <Messages />
+            </div>
           </div>
         ) : (
           <div>
             <button onClick={() => signIn("discord")}>
               Login with Discord
             </button>
+
             <div className="pt-10" />
             <Messages />
           </div>
         )}
-    </div>
-    </main >
+      </div>
+    </main>
   );
-};
+}; 
 
 export default Home;
 
